53a464e00012b8f39159f940712d2ba7c9260cf4
harsh <harshkalavadiya192@gmail.com>
2025-08-25 18:12:27 +0530

    Add ProductLoading component and integrate loading states in Home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import Box from '@mui/material/Box';
 import ProductsSlider from '../components/ProductsSlider';
 import { MyContext } from '../App';
 import { fetchDataFromApi } from '../utils/api';
+import ProductLoading from '../components/ProductLoading';
 
 const Home = () => {
   const context = useContext(MyContext);
@@ -24,6 +25,7 @@ const Home = () => {
   }
 
   useEffect(() => {
+    setPopularProductsData([])
     fetchDataFromApi(`/api/product/getAllProductsByCatId/${context?.catData[0]?._id}`).then((res) => {
       if (res?.error === false) {
         setPopularProductsData(res?.data)
@@ -33,17 +35,20 @@ const Home = () => {
   }, [context?.catData])
 
   useEffect(() => {
+    setAllProductData([])
+    setAllFeatureProducts([])
+
     fetchDataFromApi("/api/product/getAllProducts").then((res) => {
       if (res?.error === false) {
         setAllProductData(res?.products)
-        console.log(res?.products)
+        
       }
     })
 
     fetchDataFromApi("/api/product/getAllFeatureProducts").then((res) => {
       if (res?.error === false) {
         setAllFeatureProducts(res?.products)
-        console.log(res?.products)
+        
       }
     })
   }, [])
@@ -99,18 +104,10 @@ const Home = () => {
             </div>
           </div>
 
-          <div className='flex items-center animate-pulse'>
-            <div className='col w-[20%] h-[250px]'>
-              <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded-sm sm:w-96 dark:bg-gray-300">
-                <svg className="w-10 h-10 text-gray-200 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                </svg>
-              </div>
+          {
+            popularProductsData?.length === 0 && <ProductLoading />
+          }
 
-              <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-300 w-48 mb-4 mt-3"></div>
-              <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-300 max-w-[480px] mb-2.5"></div>
-            </div>
-          </div>
 
           {
             popularProductsData?.length !== 0 && <ProductsSlider items={5} data={popularProductsData} />
@@ -147,8 +144,12 @@ const Home = () => {
       <section className='py-5 pt-0 bg-white'>
         <div className='container'>
           <h2 className='text-[20px] font-[600]'>Latest Products</h2>
+
           {
-            allProductData?.length !== 0 && <ProductsSlider items={5} data={allProductData || []} />
+            allProductData?.length === 0 && <ProductLoading />
+          }
+          {
+            allProductData?.length !== 0 && <ProductsSlider items={5} data={allProductData} />
           }
 
           <AdsBannerSlider items={3} />
@@ -158,6 +159,10 @@ const Home = () => {
       <section className='py-5 pt-0 bg-white'>
         <div className='container'>
           <h2 className='text-[20px] font-[600]'>Featured Products</h2>
+          {
+            allFeatureProducts?.length === 0 && <ProductLoading />
+          }
+          
           {
             allFeatureProducts?.length !== 0 && <ProductsSlider items={5} data={allFeatureProducts} />
           }
